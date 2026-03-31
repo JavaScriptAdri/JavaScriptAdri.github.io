f0de35adab56a7b542e3745eb01363d951e951b2
Adrià Montes <tu@email.com>
2026-03-31 19:52:20 +0200

    portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // reemplaza 'portfolio' con el nombre de tu repo
   plugins: [vue()],
-  base: './',  // <- esto hace que GitHub Pages cargue correctamente los archivos
 })
